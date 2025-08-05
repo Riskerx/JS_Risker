c071484b25607eedab7408de8b147b78b31e271a
Rishikesh Sharma <b230063@nitsikkim.ac.in>
2025-08-05 16:13:52 +0000

    conversion of data

---
+++ JsBasics/conversionoperation.js
@@ -0,0 +1,17 @@
+let score = "string"
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33"==> NaN
+//"true" ==> 1
+//"NULL" ==> 0
+
+let isloggedIn = 1
+let boleaIn = Boolean(isloggedIn)
+console.log(boleaIn)
+
