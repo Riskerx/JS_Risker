a24fcf838aa36569a7e98e4324adfebfc08497f7
Rishikesh Sharma <b230063@nitsikkim.ac.in>
2025-08-05 15:59:12 +0000

    variables in js

---
+++ JsBasics/variables.js
@@ -0,0 +1,14 @@
+const account_id = 23425
+let account_email = "rishikesh@gmail.com"
+var account_password  = "23425"
+account_city = "jaipur"
+console.log(account_id)
+
+account_email = "shivam@gmail.com"
+account_password = "2636"
+account_city = "madhyapradesh"
+
+console.table([account_id, account_email, account_password, account_city])
+
+// const will not let you change the value of the variable
+// var is old thing, don't use it
