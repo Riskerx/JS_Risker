3052e4488fc5c68a043dab09373c91b271ffc13b
Rishikesh Sharma <b230063@nitsikkim.ac.in>
2025-08-05 17:03:25 +0000

    relative data type discussed

--- JsBasics/datatypes.js
+++ JsBasics/datatypes.js
@@ -1,9 +1,9 @@
 "use strict" //use all javascripts as newer js 
 // for documentation we use MDN/tc39.es
 
-/* data types
+/* permitive data types
 number
-int
+Bigint
 string = ""
 boolean = true/false
 null = standalone value, means void
@@ -11,3 +11,21 @@ undefined
 symbol= for finding uniqueness
 object*/
 console.log(typeof "rishikesh")
+
+// reference data types
+//array, objects, functions
+
+let fruits = ["mango", "apple", "banana"]
+
+let infoStudents = {
+    Name: "Dhiraj",
+    cgpa: 9.8,
+    age: 24
+
+}
+
+const myfxn = function(){
+    console.log("returnme");
+}
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
