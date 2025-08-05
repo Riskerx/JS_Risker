13e7e0b98fedf9dca96f7a5716f098f2ad3b9da1
Rishikesh Sharma <b230063@nitsikkim.ac.in>
2025-08-05 15:59:34 +0000

    data types in js

---
+++ JsBasics/datatypes.js
@@ -0,0 +1,13 @@
+"use strict" //use all javascripts as newer js 
+// for documentation we use MDN/tc39.es
+
+/* data types
+number
+int
+string = ""
+boolean = true/false
+null = standalone value, means void
+undefined
+symbol= for finding uniqueness
+object*/
+console.log(typeof "rishikesh")
