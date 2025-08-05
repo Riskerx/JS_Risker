096ac855365192da29d9775071df0fad0801c726
Rishikesh Sharma <b230063@nitsikkim.ac.in>
2025-08-05 16:44:44 +0000

    comparasions discussed

---
+++ JsBasics/comparasions.js
@@ -0,0 +1,9 @@
+console.log(2>1)
+console.log("10">13)
+console.log(null>=0)
+console.log(undefined>12)
+
+//if we use === is checks datatype before conversion
+
+console.log("13"===13)
+console.log(13===13)
