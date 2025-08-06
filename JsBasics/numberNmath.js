d4c5a88e1d5b7bd1049751c0061ee1495852b61b
Rishikesh Sharma <b230063@nitsikkim.ac.in>
2025-08-06 15:58:59 +0000

    number and math discussed

---
+++ JsBasics/numberNmath.js
@@ -0,0 +1,15 @@
+const balance = new Number(100984137.3453)
+console.log(balance)
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+console.log(balance.toPrecision(3))
+console.log(balance.toLocaleString('en-IN'))
+console.log(Number.EPSILON)
+
+//____________________________________________________________________//
+console.log(Math.abs(-4))
+console.log(Math.round(423421.254))
+console.log(Math.min(3,3543,232,263,243,647,34,8567,345,54674))
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
